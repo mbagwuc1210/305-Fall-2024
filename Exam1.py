5f9fcda9f07a08876780911cbfc101ffb1a9e692
mbagwuc1210 <mbagwuc1210@students.bowiestate.edu>
2024-10-20 19:49:14 -0400

    Update Exam1.py

--- Exam1.py
+++ Exam1.py
@@ -1,2 +1,10 @@
-for i in range(1, 101):
-    print(i)
+num1 = 15
+num2 = 7
+
+addition = num1 + num2
+subtraction = num1 - num2
+multiplication = num1 * num2
+
+print(f"Addition: {addition}")
+print(f"Subtraction: {subtraction}")
+print(f"Multiplication: {multiplication}")
